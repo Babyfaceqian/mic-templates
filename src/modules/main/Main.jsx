916436db3b45770ae970cbb63d7420d681ed75bb
babyfaceqian <babyfaceqian92@gmail.com>
2020-01-16 14:07:54 +0800

    fix: 移除antd

--- src/modules/main/Main.jsx
+++ src/modules/main/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Main.less';
-import { Button } from 'antd';
-export default class Main extends React.Component {
+
+class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {}
@@ -14,7 +14,6 @@ export default class Main extends React.Component {
     console.log('this.props', this.props);
     return (
       <div className={styles.main}>
-        <Button>ceshi</Button>
         A React Front-End Project</div>
     );
   }
